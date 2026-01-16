a79ff448f98a7f49e303541e63c57b650efdb871
ZhukDI <dmitryzhuk98@gmail.com>
2026-01-16 23:52:52 +0300

    Added compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import compress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   site: 'https://cv.dzhuk.com',
-  integrations: [sitemap()]
+  integrations: [sitemap(), compress()]
 });
